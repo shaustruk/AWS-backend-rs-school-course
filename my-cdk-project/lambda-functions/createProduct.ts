9049c681c84bc4e4f7b51b85d8f58cc4fe1a26d4
Ваше имя <es@banksiaglobal.com>
2024-06-26 00:09:30 +0200

    fix: fix creating item lambda

--- my-cdk-project/lambda-functions/createProduct.ts
+++ my-cdk-project/lambda-functions/createProduct.ts
@@ -1,30 +1,59 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
 import { DynamoDB } from 'aws-sdk';
 import { v4 as uuidv4 } from "uuid";
 
-const dynamoDb = new DynamoDB.DocumentClient();
+const client = new DynamoDBClient({});
+const dynamodb = DynamoDBDocumentClient.from(client);
+
 const PRODUCTS_TABLE_NAME = 'products';
+const STOCKS_TABLE_NAME = 'stocks';
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
   try {
-    const { title, description, price } = JSON.parse(event.body || '{}');
+    const requestBody = JSON.parse(event.body || '{}');
 
-    if (!title || !price) {
+    if (!requestBody) {
       return {
         statusCode: 400,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Methods": "POST",
+        },
         body: JSON.stringify({ message: 'Invalid product data' }),
       };
     }
+//items
+    const newProduct = {
+      id: uuidv4(),
+      title: requestBody.title,
+      description: requestBody.description || '',
+      price: requestBody.price
+    };
 
-    const id = uuidv4();
-    const newProduct = { id, title, description, price };
-
-    await dynamoDb.put({
+    const stock = {
+      product_id: requestBody.id,
+      count: requestBody.count
+    };
+//tables
+ const paramsProduct = {
       TableName: PRODUCTS_TABLE_NAME,
-      Item: newProduct,
-    }).promise();
+      Item: newProduct
+    };
+
+    const paramsStock = {
+      TableName: STOCKS_TABLE_NAME,
+      Item: stock
+    };
+    const productPutCommand = new PutCommand(paramsProduct);
+    const stockPutCommand = new PutCommand(paramsStock);
+
+    await dynamodb.send(productPutCommand);
+    await dynamodb.send(stockPutCommand);
 
     return {
       statusCode: 201,
