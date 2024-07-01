253bae18f3efeb64e6de4707ee69c78fc2382b10
Ваше имя <es@banksiaglobal.com>
2024-07-01 02:05:25 +0200

    feat: add stack

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,16 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Define the S3 bucket
+    const bucket = new s3.Bucket(this, 'ImportServiceBucket', {
+      versioned: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
+      autoDeleteObjects: true, // NOT recommended for production code
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImportServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Deploy an empty file to create the 'uploaded' folder
+    new s3deploy.BucketDeployment(this, 'DeployUploadedFolder', {
+      destinationBucket: bucket,
+      sources: [s3deploy.Source.data('uploaded/', '')],
+    });
+
+    // Output the S3 bucket name
+    new cdk.CfnOutput(this, 'BucketName', {
+      value: bucket.bucketName,
+    });
   }
 }
