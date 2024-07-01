18968a290e0c0cd553fde1fa05aba06505de6498
Ваше имя <es@banksiaglobal.com>
2024-07-01 22:01:27 +0200

    fix: fix files

--- import-service/lambda-functions/importFileParser.ts
+++ import-service/lambda-functions/importFileParser.ts
@@ -1,30 +1,48 @@
 import { S3Handler } from 'aws-lambda';
-import { S3 } from '@aws-sdk/client-s3';
-import * as csvParser from 'csv-parser';
+import { S3Client, GetObjectCommand, CopyObjectCommand, DeleteObjectCommand} from '@aws-sdk/client-s3';
 import { Readable } from 'stream';
+import * as csv from "csv-parser";
 
-const s3 = new S3({});
+// Initialize S3 client for interacting with S3 service
+const s3 = new S3Client({ region: 'eu-west-1' });
 
+// Define Lambda function handler
 export const handler: S3Handler = async (event) => {
-    const bucket = event.Records[0].s3.bucket.name;
-    const key = event.Records[0].s3.object.key;
-
-    try {
-        const s3Object = await s3.getObject({ Bucket: bucket, Key: key });
-        const s3Stream = s3Object.Body as Readable;
-
-        s3Stream
-            .pipe(csvParser())
-            .on('data', (data) => {
-                console.log('Parsed record:', data);
-            })
-            .on('end', () => {
-                console.log('CSV parsing completed.');
-            })
-            .on('error', (error) => {
-                console.error('Error parsing CSV:', error);
+    // Loop through all records in the event
+    for (const record of event.Records) {
+        const { bucket, object } = record.s3;
+        const sourceKey = object.key;
+        const parsedKey = sourceKey.replace('uploaded/', 'parsed/');
+        const params = {
+            Bucket: bucket.name,
+            Key: sourceKey,
+        };
+        try {
+            // Get the object from S3
+            const getCommand = new GetObjectCommand(params);
+            const copyCommand = new CopyObjectCommand({
+                Bucket: bucket.name,
+                CopySource: `${bucket.name}/${sourceKey}`,
+                Key: parsedKey,
             });
-    } catch (error) {
-        console.error('Error processing S3 event:', error);
+          
+            const deleteCommand = new DeleteObjectCommand(params);
+        
+            const { Body } = await s3.send(getCommand);            
+            // Convert Body to Readable stream
+            const stream = Body as Readable;
+            // Parse CSV
+            stream.pipe(csv())
+                .on('data', (data) => console.log('Parsed record:', data))
+                .on('end', () => console.log('Parsing completed.'));
+
+            // Copy and delete the original file
+            await s3.send(copyCommand);
+            await s3.send(deleteCommand);
+            console.log('CSV moved from "uploaded" to "parsed" directory and removed in "uploaded"');
+                
+        } catch (error) {
+            console.error(`Error processing ${object.key} from ${bucket.name}`, error);
+        }
     }
 };
--- import-service/lambda-functions/importProductsFile.ts
+++ import-service/lambda-functions/importProductsFile.ts
@@ -2,21 +2,25 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
-const s3Client = new S3Client({ region: 'us-east-1' }); // Specify your region
+const s3Client = new S3Client({ region: 'us-east-1' });
 
-const headers = {
-    "Access-Control-Allow-Origin": "*",
+const headers =  {
+    "Access-Control-Allow-Origin": "*",            
     "Access-Control-Allow-Methods": "*",
-    "Access-Control-Allow-Headers": "Content-Type,Authorization,X-Api-Key,X-Amz-Date,X-Amz-Security-Token",
-};
+    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+  },
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-    const bucketName = process.env.BUCKET_NAME;  // Use environment variable for bucket name
+    if (event.httpMethod === 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers,
+            body: JSON.stringify({ message: 'CORS preflight' }),
+        };
+    }
+
+    const bucketName = 'ImportServiceBucket';
     const fileName = event.queryStringParameters?.name;
-
-    console.log('cutrent file:', fileName);
-
-    console.log('bucket', bucketName);
 
     if (!bucketName) {
         return {
@@ -37,11 +41,10 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     try {
         const command = new PutObjectCommand({
             Bucket: bucketName,
-            Key: `uploaded/${fileName}`,
-            ContentType: 'text/csv'
+            Key: `uploaded/${fileName}`,   
         });
 
-        const url = await getSignedUrl(s3Client, command); 
+        const url = await getSignedUrl(s3Client, command);
 
         return {
             statusCode: 200,
